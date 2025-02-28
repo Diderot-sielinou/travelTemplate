a4f1d84e00426885550c8febc10f32db051f4356
diderot-sielinou <diderotsielinou@gmail.com>
2025-02-28 19:55:16 +0100

    finish review-navigation

--- src/components/Hearder.jsx
+++ src/components/Hearder.jsx
@@ -1,7 +1,8 @@
-import { useNavigate } from "react-router";
+
+import { Link } from "react-router";
 
 export default function Hearder() {
-  let navigate = useNavigate();
+
   return (
     <>
       <nav className="">
@@ -38,7 +39,7 @@ export default function Hearder() {
           <div className="logo d-flex align-items-center">
             <div className="img-logo">
               <img
-                src="https://s3-alpha-sig.figma.com/img/7b44/3779/e00233c67a961c8bfdf2e37a4a1ceb1b?Expires=1739750400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=kSucIDsCJ4YRwWgVpTDmWihHBBih6D-PEKWVyyCkezgGGHoLoDpD7fjgePAMnNtDsSeuTdHCgxt5F~3po6wOfglP~6oiHUxd-9WSdID~8xyaEbtsY96Lj3B0UTx8Q3kpDI40e3UGeDdn7pskddHJcNnUnSmQ7QYdjuDyaqWQrVLlA7ArUCVAWyprCJ4nuW3wfgjC0XxqcmNgWUPpLELbtwr1PHJD4xLi2ae5hto0rDOWhyDxrN73CLuLcGGiK0E6VyxLBBm9qbFLuCXUlzCa62prEucle5t52rbwa4SpQgvkHxd4CAB0qgFk9XzR5b99DWBF9lI0j-TX1ZapxLaW9A__"
+                src="https://s3-alpha-sig.figma.com/img/7b44/3779/e00233c67a961c8bfdf2e37a4a1ceb1b?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nZvHZDGhrEn8fO~B-UrrPF3rR3YC1LDOl5yjBGHY-XC8dvcr7KvR3g6OFULOUHYwPJxi-oq-YSL3SolVgZV9j0Qbz3EjxhIAhBB~jTbQArb9N9LHpJvSYMbJBDZ3oicFjWf8zdoXD~ZeXTf3yJUhfvyCnpwz~hzrgpCRoMAYQbdrqdB2VNvKh8cJehsN-XBtTslfx7HjhBbkJwll~Js7scLiPzPmN-rp8PqIRB~fjCQL0xgqocI0~LnVpYAwXhLjBUP~ketSartDVM2t4esVa8l8XBPNLkdLheHGb2XbyfxcSY692DHN8C6rVoPi7Ko7Eh3ZggbbU5AwES1n10Z50Q__"
                 alt="logo"
               />
             </div>
@@ -46,19 +47,23 @@ export default function Hearder() {
               <ul className="nav-list d-flex align-items-center ">
                 <li className="nav-item">
                 {/* <Link to="/">Login again</Link> */}
-                  <a onClick={()=> navigate("/")}>Home</a>
+                <Link to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                  <a onClick={()=> navigate("About")} >About</a>
+                <Link to="About">About</Link>
+
                 </li>
                 <li className="nav-item">
-                  <a onClick={()=> navigate("package")} >Tour package</a>
+
+                  <Link to="package">Tour package</Link>
                 </li>
                 <li className="nav-item">
-                  <a onClick={()=> navigate("galery")} >Gallery Blog</a>
+
+                  <Link to="galery">Gallery Blog</Link>
                 </li>
                 <li className="nav-item">
-                  <a onClick={()=> navigate("contact")} >Contact Us</a>
+
+                  <Link to="Contact">Contact Us</Link>
                 </li>
               </ul>
             </div>
